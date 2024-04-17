0afaaa17daf0fb8835023a64a73e42b6ad0954e9
21F9618 <zhalaym@gmail.com>
2024-04-17 16:25:33 +0500

    signup and signin toggel

---
+++ controllers/formToggle.js
@@ -0,0 +1,9 @@
+function toggleForms() {
+    var signupForm = document.getElementById('signup-form');
+    var loginForm = document.getElementById('login-form');
+    
+    // Toggle visibility
+    signupForm.classList.toggle('hidden');
+    loginForm.classList.toggle('hidden');
+  }
+  
