8d49e0608843092814bf82a35ca37538fa42033b
kamlasafdar <kamlasafdar23@gmail.com>
2024-05-16 00:47:32 +0500

    added page controller

--- controllers/formToggle.js
+++
@@ -1,9 +0,0 @@
-function toggleForms() {
-    var signupForm = document.getElementById('signup-form');
-    var loginForm = document.getElementById('login-form');
-    
-    // Toggle visibility
-    signupForm.classList.toggle('hidden');
-    loginForm.classList.toggle('hidden');
-  }
-  
